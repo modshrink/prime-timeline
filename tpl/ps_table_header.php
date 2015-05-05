f594a650d3593ae1b3f21a3e0bd83d5d6040cb27
mayoibi <mayoibi@modshrink.com>
2015-05-06 04:16:07 +0900

    fix: マージミス

--- tpl/ps_table_header.php
+++ tpl/ps_table_header.php
@@ -1,18 +1,7 @@
-<p><?php echo sprintf( 'Total execution time (%.2f msec)', $exec); ?></p>
-<table class="wp-list-table widefat fixed pages">
 <p><?php echo sprintf( __( 'Total execution time (%.2f msec)', 'prime-timeline' ), $exec ); ?></p>
 <table class="wp-list-table widefat fixed pages">
 	<tbody>
 		<tr id='ps_tr_head'>
-			<th width='5%'>No.</th>
-			<th width='5%'>Total<br />(msec)</th>
-			<th width='10%'>Type</th>
-			<th width='5%'>File</th>
-			<th width='30%'>Hook/SQL</th>
-			<th width='30%'>Detail</th>
-			<th width='5%'>Count</th>
-			<th width='5%'>Diff<br />(msec)</th>
-			<th width='5%'>This<br />(msec)</th>
 			<th width='5%'><?php _e( 'No.', 'prime-timeline' ); ?></th>
 			<th width='5%'><?php _e( 'Total', 'prime-timeline' ); ?><br />(msec)</th>
 			<th width='10%'><?php _e( 'Type', 'prime-timeline' ); ?></th>
