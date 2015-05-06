c86f620ba4ee68b017452e64c7429aa29e2bff9b
mayoibi <mayoibi@modshrink.com>
2015-05-07 04:34:07 +0900

    delete: 開閉用の個別ボタン廃止

--- tpl/ps_panel.php
+++ tpl/ps_panel.php
@@ -2,37 +2,25 @@
 	<div id="ps_head">
 		<ul>
 			<li><ul id="ps_all">
-				<li class="ps_filter"><a href="#"><?php _e( 'All Timeline', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'All Timeline', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_file">
-				<li class="ps_filter"><a href="#"><?php _e( 'Loaded Files', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'Loaded Files', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_sql">
-				<li class="ps_filter"><a href="#"><?php _e( 'SQL Queries', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'SQL Queries', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook">
-				<li class="ps_filter"><a href="#"><?php _e( 'All Hooks', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'All Hooks', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook_do">
-				<li class="ps_filter"><a href="#"><?php _e( 'Registerd Hooks', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'Registerd Hooks', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook_pass">
-				<li class="ps_filter"><a href="#"><?php _e( 'Passed Hooks', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'Passed Hooks', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_slow">
-				<li class="ps_filter"><a href="#"><?php _e( 'Slow Points', 'prime-timeline' ); ?></a></li>
-				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
-				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
+				<li class="ps_filter"><a class="button button-primary button-small" href="#"><?php _e( 'Slow Points', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul>
 				<li><input type='text' id='ps_slow_search' value='10' /></li>
